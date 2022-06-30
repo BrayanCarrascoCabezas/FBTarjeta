f10af87e52e2771fbb2b4d5f486dd8efd29e0245
BrayanCarrascoCabezas <bryandavid619@gmail.com>
2022-06-29 22:58:16 -0500

    Agregar archivos de proyecto.

---
+++ FBTarjeta/AplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FBTarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjeta
+{
+    public class AplicationDbContext: DbContext
+    {
+       public  DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) { 
+        
+        }
+    }
+}
---
+++ FBTarjeta/Migrations/20220628044745_Brayan.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FBTarjeta.Migrations
+{
+    public partial class Brayan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
